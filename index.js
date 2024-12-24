8f691b0e78027fb815a803f3269f8cf7ead3e081
jits98 <jits8809@gmail.com>
2024-12-24 17:39:19 +0530

    wiring button click

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 let userName = prompt("Write your name", "Name");
 
 alert("Welcome " + userName);
+
+let button = document.querySelector(".js-btn");
+
+button.addEventListener("click", clickHandler);
+
+function clickHandler() {
+  console.log("clicked");
+}
