f1635305639fdacbe5b7280267db4b60d0272f08
jits98 <jits8809@gmail.com>
2024-12-24 18:15:23 +0530

    wiring a div to show output

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
 let button = document.querySelector(".js-btn");
 let inputElement = document.querySelector(".txtarea");
 
+let outputElement = document.querySelector(".output");
+
 button.addEventListener("click", clickHandler);
 
 function clickHandler() {
   console.log("clicked");
   console.log(inputElement.value);
+  outputElement.innerText = "I am Iron-Man";
 }
-
-//document.querySelector('textarea')
-//document.querySelector('.btn-primary')
-//document.querySelector('#input-btn')
-//document.querySelector('input[type='translator']')
