e05cd86579ac24d1f03016efbb06c7d3fc3c6e7c
jits98 <jits8809@gmail.com>
2024-12-24 17:51:43 +0530

    wiring textarea input

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
-let userName = prompt("Write your name", "Name");
-
-alert("Welcome " + userName);
-
 let button = document.querySelector(".js-btn");
+let inputElement = document.querySelector(".txtarea");
 
 button.addEventListener("click", clickHandler);
 
 function clickHandler() {
   console.log("clicked");
+  console.log(inputElement.value);
 }
