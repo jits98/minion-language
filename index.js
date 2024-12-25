f1cc6c04fcc26859f06102293062bbbb48a88d0e
jits98 <jits8809@gmail.com>
2024-12-25 12:03:39 +0530

    Add api for translation

--- index.js
+++ index.js
@@ -1,12 +1,28 @@
 let button = document.querySelector(".js-btn");
 let inputElement = document.querySelector(".txtarea");
-
 let outputElement = document.querySelector(".output");
 
-button.addEventListener("click", clickHandler);
+let serverURL = "https://api.funtranslations.com/translate/minion.json";
+
+function getTranslationURL(text) {
+  return serverURL + "?" + "text=" + text;
+}
+
+function errorHandler(error) {
+  console.log("error occured", error);
+  alert("something wrong with server! Try again after some time");
+}
 
 function clickHandler() {
-  console.log("clicked");
-  console.log(inputElement.value);
-  outputElement.innerText = "I am Iron-Man";
+  let inputText = inputElement.value;
+
+  fetch(getTranslationURL(inputText))
+    .then((response) => response.json())
+    .then((json) => {
+      let translatedText = json.contents.translated;
+      outputElement.innerText = translatedText;
+    })
+    .catch(errorHandler);
 }
+
+button.addEventListener("click", clickHandler);
